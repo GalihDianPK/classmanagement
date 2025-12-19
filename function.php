d739e896408e8a0da6baa6f44307eff214917fcc
GalihDianPK <galihdpk0204@gmail.com>
2025-12-19 21:25:59 +0700

    penambahan fitur tambah matkul

--- function.php
+++ function.php
@@ -3,4 +3,19 @@ session_start();
 
 $conn = mysqli_connect("localhost", "root", "", "classmanagement"); //? connect database
 
-//? memasukkan data matak
+//? memasukkan data matakuliah baru
+if(isset($_POST['addnewmatkul'])){
+    $idmatkul = $_POST['idmatkul'];
+    $namamatkul = $_POST['namamatkul'];
+    $tanggal = $_POST['tanggal'];
+    $namadosen = $_POST['namadosen'];
+    $jammatkul = $_post['jammatkul'];
+
+    $addtotable = mysqli_query($conn, "INSERT into matkul (idmatkul, namamatkul, tanggal, namadosen, jammatkul) values('$idmatkul', '$namamatkul', '$tanggal', '$namadosen', '$jammatkul')");
+    if($addtotable) {
+        header('Location: matakuliah.php');
+    } else {
+        echo "Gagal";
+        header('Location: matakuliah.php');
+    }
+}
